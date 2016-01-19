be737748f09a109b6b9e418fbcd402d67dc091c5
Manabu Shimada <manabu.shimada@mac.com>
2016-01-19 21:32:32 +0000

    added ShakeDelay project

---
+++ ShakeDelay/ShakeDelay/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ShakeDelay
+//
+//  Created by manabu shimada on 19/01/2016.
+//  Copyright © 2016 Asio Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
